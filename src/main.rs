f423f6d617d6290d176e187e251a9cfa2cc81fd0
c0nD <colintiller21@gmail.com>
2023-11-12 22:14:56 -0500

    updated to having a working resume builder prototype in rust

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,105 @@
-use printpdf::*;
 use std::io::{self, Write};
-use std::fs::File;
 use std::path::Path;
 
 mod resume_builder;
 use resume_builder::ResumeBuilder;
 
 fn main() {
-    let name = prompt("Enter your name: ");
-    let education = prompt("Enter your education: ");
-    let experience = prompt("Enter your experience: ");
-    let skills = prompt("Enter your skills: ");
-    let filename = prompt("Enter filename for your resume (e.g., resume.pdf): ");
+    // Basic Info
+    let name = prompt("Enter your full name: ");
+    let phone = prompt("Enter your phone number: ");
+    let email = prompt("Enter your email address: ");
+    let location = prompt("Enter your location (city/state): ");
 
+    // Education
+    let mut education_entries = Vec::new();
+    println!("Enter your education details (type 'Done' when finished):");
+    loop {
+        let degree = prompt("Degree (e.g., B.S. in Computer Science): ");
+        if degree.eq_ignore_ascii_case("done") {
+            break;
+        }
+        let school = prompt("School name: ");
+        let date = prompt("Graduation date (month/year): ");
+        let gpa = prompt("GPA (optional, leave blank if not applicable): ");
+        education_entries.push((degree, school, date, gpa));
+    }
+
+    // Skills
+    let skills_input = prompt("Enter a comma separated list of your skills: ");
+    let skills = skills_input.split(',').map(|s| s.trim().to_string()).collect::<Vec<String>>();
+
+    // Projects
+    let mut projects = Vec::new();
+    println!("Enter details for each project you want to include (type 'Done' when finished):");
+    loop {
+        let project_name = prompt("Project name: ");
+        if project_name.eq_ignore_ascii_case("done") {
+            break;
+        }
+        let role = prompt("Your role in the project: ");
+        let start_date = prompt("Project start date (month/year): ");
+        let end_date = prompt("Project end date (month/year) or 'Present': ");
+        let location = prompt("Location: ");
+        let mut bullet_points = Vec::new();
+        println!("Enter bullet points for this project (type 'Done' for each bullet point to finish):");
+        loop {
+            let bullet = prompt("Bullet point: ");
+            if bullet.eq_ignore_ascii_case("done") {
+                break;
+            }
+            bullet_points.push(bullet);
+        }
+        projects.push((project_name, role, start_date, end_date, location, bullet_points));
+    }
+
+    // Work Experience
+    let mut experiences = Vec::new();
+    println!("Enter details for each position of your work experience (type 'Done' when finished):");
+    loop {
+        let job_title = prompt("Job title: ");
+        if job_title.eq_ignore_ascii_case("done") {
+            break;
+        }
+        let company_name = prompt("Company name: ");
+        let start_date = prompt("Start date (month/year): ");
+        let end_date = prompt("End date (month/year) or 'Present': ");
+        let job_location = prompt("Location: ");
+        let mut job_bullet_points = Vec::new();
+        println!("Enter bullet points for this position (type 'Done' for each bullet point to finish):");
+        loop {
+            let bullet = prompt("Bullet point: ");
+            if bullet.eq_ignore_ascii_case("done") {
+                break;
+            }
+            job_bullet_points.push(bullet);
+        }
+        experiences.push((job_title, company_name, start_date, end_date, job_location, job_bullet_points));
+    }
+
+    // Resume Filename
+    let filename = prompt("Enter filename for your resume (e.g., 'resume.pdf'): ");
+
+    // Construct the resume
     let resume = ResumeBuilder::new()
         .name(&name)
-        .education(&education)
-        .experience(&experience)
+        .phone(&phone)
+        .email(&email)
+        .location(&location)
+        .education_entries(&education_entries)
         .skills(&skills)
+        .projects(&projects)
+        .work_experience(&experiences)
         .build();
 
+    // Save the resume to a file
     resume.save(Path::new(&filename));
+    println!("Resume saved to {}", filename);
 }
 
 fn prompt(message: &str) -> String {
     let mut input = String::new();
-    print!("{}", message);
+    print!("{} ", message);
     io::stdout().flush().unwrap();
     io::stdin().read_line(&mut input).expect("Failed to read line");
     input.trim().to_string()
--- src/resume_builder.rs
+++ src/resume_builder.rs
@@ -1,22 +1,30 @@
 use printpdf::*;
-use std::path::Path;
-use std::io::BufWriter;
 use std::fs::File;
+use std::io::BufWriter;
+use std::path::Path;
 
 pub struct ResumeBuilder {
     name: String,
-    education: String,
-    experience: String,
-    skills: String,
+    phone: String,
+    email: String,
+    location: String,
+    education_entries: Vec<(String, String, String, String)>,
+    skills: Vec<String>,
+    projects: Vec<(String, String, String, String, String, Vec<String>)>,
+    work_experience: Vec<(String, String, String, String, String, Vec<String>)>,
 }
 
 impl ResumeBuilder {
     pub fn new() -> Self {
         ResumeBuilder {
             name: String::new(),
-            education: String::new(),
-            experience: String::new(),
-            skills: String::new(),
+            phone: String::new(),
+            email: String::new(),
+            location: String::new(),
+            education_entries: Vec::new(),
+            skills: Vec::new(),
+            projects: Vec::new(),
+            work_experience: Vec::new(),
         }
     }
 
@@ -25,61 +33,133 @@ impl ResumeBuilder {
         self
     }
 
-    pub fn education(mut self, education: &str) -> Self {
-        self.education = education.to_string();
+    pub fn phone(mut self, phone: &str) -> Self {
+        self.phone = phone.to_string();
         self
     }
 
-    pub fn experience(mut self, experience: &str) -> Self {
-        self.experience = experience.to_string();
+    pub fn email(mut self, email: &str) -> Self {
+        self.email = email.to_string();
         self
     }
 
-    pub fn skills(mut self, skills: &str) -> Self {
-        self.skills = skills.to_string();
+    pub fn location(mut self, location: &str) -> Self {
+        self.location = location.to_string();
+        self
+    }
+
+    pub fn education_entries(mut self, entries: &[(String, String, String, String)]) -> Self {
+        self.education_entries = entries.to_vec();
+        self
+    }
+
+    pub fn skills(mut self, skills: &[String]) -> Self {
+        self.skills = skills.to_vec();
+        self
+    }
+
+    pub fn projects(mut self, projects: &[(String, String, String, String, String, Vec<String>)]) -> Self {
+        self.projects = projects.to_vec();
+        self
+    }
+
+    pub fn work_experience(mut self, experience: &[(String, String, String, String, String, Vec<String>)]) -> Self {
+        self.work_experience = experience.to_vec();
         self
     }
 
     pub fn build(self) -> Resume {
         Resume {
             name: self.name,
-            education: self.education,
-            experience: self.experience,
+            phone: self.phone,
+            email: self.email,
+            location: self.location,
+            education_entries: self.education_entries,
             skills: self.skills,
+            projects: self.projects,
+            work_experience: self.work_experience,
         }
     }
 }
 
 pub struct Resume {
     name: String,
-    education: String,
-    experience: String,
-    skills: String,
+    phone: String,
+    email: String,
+    location: String,
+    education_entries: Vec<(String, String, String, String)>,
+    skills: Vec<String>,
+    projects: Vec<(String, String, String, String, String, Vec<String>)>,
+    work_experience: Vec<(String, String, String, String, String, Vec<String>)>,
 }
 
 impl Resume {
-    pub fn save(self, path: &Path) {
+    pub fn save(&self, path: &Path) {
         let (doc, page1, layer1) = PdfDocument::new("Resume", Mm(210.0), Mm(297.0), "Layer 1");
         let current_layer = doc.get_page(page1).get_layer(layer1);
         let font = doc.add_builtin_font(BuiltinFont::Helvetica).unwrap();
 
-        let mut y_position = 270.0;
+        // Define initial y_position (top of the page)
+        let mut y_position = 297.0 - 20.0; // Page height - top margin
+        let left_margin = 20.0;
+        let line_height = 6.0;
+        let font_size_header = 18.0;
+        let font_size_section_title = 16.0;
+        let font_size_text = 12.0;
 
-        // Name
-        current_layer.use_text(self.name, 20.0, Mm(10.0), Mm(y_position), &font);
-        y_position -= 30.0;
+        // Helper function to add text to the layer
+        let mut add_text = |text: &str, size: f64, y_pos: &mut f64, is_title: bool| {
+            let font_size = if is_title { font_size_section_title as f32 } else { size as f32 };
+            current_layer.use_text(text.to_string(), font_size, Mm(left_margin as f32), Mm(*y_pos as f32), &font);
+            *y_pos -= if is_title { line_height * 2.0 } else { line_height }; // Double space before titles, single space otherwise
+        };
 
-        // Education
-        current_layer.use_text(format!("Education: {}", self.education), 14.0, Mm(10.0), Mm(y_position), &font);
-        y_position -= 20.0;
+        // Name and Contact Information
+        add_text(&self.name, font_size_header, &mut y_position, false);
+        add_text(&format!("{} | {}", self.phone, self.email), font_size_text, &mut y_position, false);
+        add_text(&self.location, font_size_text, &mut y_position, false);
 
-        // Experience
-        current_layer.use_text(format!("Experience: {}", self.experience), 14.0, Mm(10.0), Mm(y_position), &font);
-        y_position -= 20.0;
+        // Add a section header for Education
+        add_text("EDUCATION", font_size_section_title, &mut y_position, true);
 
-        // Skills
-        current_layer.use_text(format!("Skills: {}", self.skills), 14.0, Mm(10.0), Mm(y_position), &font);
+        // Iterate over education entries and add them to the PDF
+        for (degree, school, date, gpa) in &self.education_entries {
+            add_text(&format!("{} - {}, {}", degree, school, date), font_size_text, &mut y_position, false);
+            if !gpa.is_empty() {
+                add_text(&format!("GPA: {}", gpa), font_size_text, &mut y_position, false);
+            }
+        }
 
+        // Add a section header for Skills
+        add_text("SKILLS", font_size_section_title, &mut y_position, true);
+        add_text(&self.skills.join(", "), font_size_text, &mut y_position, false);
+
+        // Add a section header for Projects
+        add_text("PROJECTS", font_size_section_title, &mut y_position, true);
+        for (project_name, role, start_date, end_date, location, bullet_points) in &self.projects {
+            add_text(project_name, font_size_text, &mut y_position, false);
+            add_text(role, font_size_text, &mut y_position, false);
+            add_text(&format!("{} - {}", start_date, end_date), font_size_text, &mut y_position, false);
+            add_text(location, font_size_text, &mut y_position, false);
+            for point in bullet_points {
+                add_text(&format!("• {}", point), font_size_text, &mut y_position, false);
+            }
+        }
+
+        // Add a section header for Work Experience
+        add_text("WORK EXPERIENCE", font_size_section_title, &mut y_position, true);
+        for (job_title, company_name, start_date, end_date, job_location, job_bullet_points) in &self.work_experience {
+            add_text(job_title, font_size_text, &mut y_position, false);
+            add_text(company_name, font_size_text, &mut y_position, false);
+            add_text(&format!("{} - {}", start_date, end_date), font_size_text, &mut y_position, false);
+            add_text(job_location, font_size_text, &mut y_position, false);
+            for point in job_bullet_points {
+                add_text(&format!("• {}", point), font_size_text, &mut y_position, false);
+            }
+        }
+
+        // Save the PDF
         doc.save(&mut BufWriter::new(File::create(path).unwrap())).unwrap();
+        println!("Resume saved as {:?}", path);
     }
 }
