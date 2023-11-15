b7cce51c37550b7f7aaf0d5c2a03e6cf2ecdbeec
c0nD <colintiller21@gmail.com>
2023-11-15 16:52:04 -0500

    updated such that it formats the resume with fonts, bold, etc

--- src/resume_builder.rs
+++ src/resume_builder.rs
@@ -1,7 +1,5 @@
 use printpdf::*;
-use std::fs::File;
-use std::io::BufWriter;
-use std::path::Path;
+use std::{fs::File, io::BufWriter, path::Path};
 
 pub struct ResumeBuilder {
     name: String,
@@ -97,7 +95,6 @@ impl Resume {
     pub fn save(&self, path: &Path) {
         let (doc, page1, layer1) = PdfDocument::new("Resume", Mm(210.0), Mm(297.0), "Layer 1");
         let current_layer = doc.get_page(page1).get_layer(layer1);
-        let font = doc.add_builtin_font(BuiltinFont::Helvetica).unwrap();
 
         // Define initial y_position (top of the page)
         let mut y_position = 297.0 - 20.0; // Page height - top margin
@@ -108,53 +105,59 @@ impl Resume {
         let font_size_text = 12.0;
 
         // Helper function to add text to the layer
-        let mut add_text = |text: &str, size: f64, y_pos: &mut f64, is_title: bool| {
-            let font_size = if is_title { font_size_section_title as f32 } else { size as f32 };
-            current_layer.use_text(text.to_string(), font_size, Mm(left_margin as f32), Mm(*y_pos as f32), &font);
-            *y_pos -= if is_title { line_height * 2.0 } else { line_height }; // Double space before titles, single space otherwise
+        let mut add_text = |text: &str, size: f64, y_pos: &mut f64, is_bold: bool, new_section: bool| {
+            if new_section {
+                *y_pos -= line_height * 2.0; // Add extra space before new section
+            }
+            let font = if is_bold {
+                doc.add_builtin_font(BuiltinFont::HelveticaBold).unwrap()
+            } else {
+                doc.add_builtin_font(BuiltinFont::Helvetica).unwrap()
+            };
+            let text_len = (text.len() as f64 * (size / 2.0)).round(); // Rough estimate of text length
+            if text_len + left_margin > 210.0 - left_margin { // Check if text exceeds the margin
+                *y_pos -= line_height; // Move to new line if it does
+            }
+            current_layer.use_text(text.to_string(), size as f32, Mm(left_margin as f32), Mm(*y_pos as f32), &font);
+            *y_pos -= line_height;
         };
 
         // Name and Contact Information
-        add_text(&self.name, font_size_header, &mut y_position, false);
-        add_text(&format!("{} | {}", self.phone, self.email), font_size_text, &mut y_position, false);
-        add_text(&self.location, font_size_text, &mut y_position, false);
+        add_text(&self.name, font_size_header, &mut y_position, true, false);
+        add_text(&format!("{} | {}", self.phone, self.email), font_size_text, &mut y_position, false, false);
+        add_text(&self.location, font_size_text, &mut y_position, false, false);
 
         // Add a section header for Education
-        add_text("EDUCATION", font_size_section_title, &mut y_position, true);
+        add_text("EDUCATION", font_size_section_title, &mut y_position, true, true);
 
         // Iterate over education entries and add them to the PDF
         for (degree, school, date, gpa) in &self.education_entries {
-            add_text(&format!("{} - {}, {}", degree, school, date), font_size_text, &mut y_position, false);
-            if !gpa.is_empty() {
-                add_text(&format!("GPA: {}", gpa), font_size_text, &mut y_position, false);
-            }
+            add_text(&format!("{} - {}", degree, school), font_size_text, &mut y_position, false, false);
+            add_text(&format!("{} | GPA: {}", date, gpa), font_size_text, &mut y_position, false, false);
         }
 
         // Add a section header for Skills
-        add_text("SKILLS", font_size_section_title, &mut y_position, true);
-        add_text(&self.skills.join(", "), font_size_text, &mut y_position, false);
+        add_text("SKILLS", font_size_section_title, &mut y_position, true, true);
+        add_text(&self.skills.join(", "), font_size_text, &mut y_position, false, false);
 
         // Add a section header for Projects
-        add_text("PROJECTS", font_size_section_title, &mut y_position, true);
-        for (project_name, role, start_date, end_date, location, bullet_points) in &self.projects {
-            add_text(project_name, font_size_text, &mut y_position, false);
-            add_text(role, font_size_text, &mut y_position, false);
-            add_text(&format!("{} - {}", start_date, end_date), font_size_text, &mut y_position, false);
-            add_text(location, font_size_text, &mut y_position, false);
+        add_text("PROJECTS", font_size_section_title, &mut y_position, true, true);
+        for (project_name, role, start_date, end_date, description, bullet_points) in &self.projects {
+            add_text(&format!("{} | {}", project_name, role), font_size_text, &mut y_position, true, false);
+            add_text(&format!("{} - {}", start_date, end_date), font_size_text, &mut y_position, false, false);
+            add_text(description, font_size_text, &mut y_position, false, false);
             for point in bullet_points {
-                add_text(&format!("• {}", point), font_size_text, &mut y_position, false);
+                add_text(&format!("• {}", point), font_size_text, &mut y_position, false, false);
             }
         }
 
         // Add a section header for Work Experience
-        add_text("WORK EXPERIENCE", font_size_section_title, &mut y_position, true);
+        add_text("WORK EXPERIENCE", font_size_section_title, &mut y_position, true, true);
         for (job_title, company_name, start_date, end_date, job_location, job_bullet_points) in &self.work_experience {
-            add_text(job_title, font_size_text, &mut y_position, false);
-            add_text(company_name, font_size_text, &mut y_position, false);
-            add_text(&format!("{} - {}", start_date, end_date), font_size_text, &mut y_position, false);
-            add_text(job_location, font_size_text, &mut y_position, false);
+            add_text(company_name, font_size_text, &mut y_position, true, false);
+            add_text(&format!("{} | {} - {}", job_title, start_date, end_date), font_size_text, &mut y_position, false, false);
             for point in job_bullet_points {
-                add_text(&format!("• {}", point), font_size_text, &mut y_position, false);
+                add_text(&format!("• {}", point), font_size_text, &mut y_position, false, false);
             }
         }
 
